bdd6f256d6aebf397b136e9869df2fcd9d0470c8
K2SO <24651269+K2SO@users.noreply.github.com>
2019-02-23 06:53:32 -0500

    A bunch
    Read/write input image to storage directory
    Set visibility of image and its caption on command
    Mild Layout update

--- app/src/main/java/com/hackja/nbths/vsuck/MainActivity.java
+++ app/src/main/java/com/hackja/nbths/vsuck/MainActivity.java
@@ -1,13 +1,24 @@
 package com.hackja.nbths.vsuck;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,20 +30,48 @@ public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
+    private static File lastPhoto = null;
+
     public void OpenCam(View view) {
+        ImageView img = findViewById(R.id.thumbnail);
+        TextView descriptor = findViewById(R.id.your_thumbnail);
+        img.setVisibility(View.INVISIBLE);
+        descriptor.setVisibility(View.INVISIBLE);
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "JPEG_" + timeStamp + "_";
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            try {
+                File image = File.createTempFile(
+                        imageFileName,  /* prefix */
+                        ".jpg",         /* suffix */
+                        storageDir      /* directory */
+                );
+                lastPhoto = image;
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.hackja.nbths.vsuck.fileprovider",
+                        image);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } catch (IOException io) {}
         } else Toast.makeText(getApplicationContext(),"Couldn't find a camera app",Toast.LENGTH_LONG).show();
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            Bitmap imageBitmap = BitmapFactory.decodeFile(lastPhoto.getAbsolutePath());
             ImageView img = findViewById(R.id.thumbnail);
             img.setImageBitmap(imageBitmap);
+            TextView descriptor = findViewById(R.id.your_thumbnail);
+            descriptor.setText("We're having trouble identifying this image");
+
+            img.setVisibility(View.VISIBLE);
+            descriptor.setVisibility(View.VISIBLE);
+
         }
     }
 }
