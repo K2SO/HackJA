d6ff0d918d941a11dd90c0d0429d5aba81381ca5
K2SO <24651269+K2SO@users.noreply.github.com>
2019-02-22 22:37:25 -0500

    Open Camera On-click

--- app/src/main/java/com/hackja/nbths/vsuck/MainActivity.java
+++ app/src/main/java/com/hackja/nbths/vsuck/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hackja.nbths.vsuck;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void OpenCam(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } else Toast.makeText(getApplicationContext(),"Couldn't find a camera app",Toast.LENGTH_LONG).show();
     }
 }
